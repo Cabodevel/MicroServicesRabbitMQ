89f17dccbe4d1b649ddcf2738daa6d709d0154ee
Carlos <carlosbolorino@hotmail.com>
2022-05-15 20:42:43 +0200

    setting rabbitMQ

---
+++ MicroRabbit.Infra.Bus/RabbitMqBus.cs
@@ -0,0 +1,60 @@
+﻿using MediatR;
+using MicroRabbit.Domain.Core.Bus;
+using MicroRabbit.Domain.Core.Commands;
+using MicroRabbit.Domain.Core.Events;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+using System.Text;
+
+namespace MicroRabbit.Infra.Bus
+{
+    public sealed class RabbitMqBus : IEventBus
+    {
+        private readonly RabbitMqSettings _settings;
+        private readonly IMediator _mediator;
+        private readonly new Dictionary<string, List<Type>> _handlers;
+        private readonly List<Type> _eventTypes;
+        public RabbitMqBus(IMediator mediator, 
+            IOptions<RabbitMqSettings> settings)
+        {
+            _mediator = mediator;
+            _settings = settings.Value;
+            _handlers = new Dictionary<string, List<Type>>();
+            _eventTypes = new List<Type>();
+        }
+
+        public void Publish<T>(T @event) where T : Event
+        {
+            var factory = new ConnectionFactory
+            {
+                HostName = _settings.HostName,
+                UserName = _settings.UserName,
+                Password = _settings.Password,
+            };
+
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+
+            var eventName = @event.GetType().Name;
+            channel.QueueDeclare(eventName);
+            var message = JsonConvert.SerializeObject(@event);
+            var body = Encoding.UTF8.GetBytes(message);
+
+            channel.BasicPublish("", eventName, null, body);
+
+        }
+
+        public Task SendCommand<T>(T command) where T : Command
+        {
+            return _mediator.Send(command);
+        }
+
+        public void Subscribe<T, TH>()
+            where T : Event
+            where TH : IEventHandler<T>
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
