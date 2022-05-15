8406f8e671040449469fb8fbc698515a905668eb
Carlos <carlosbolorino@hotmail.com>
2022-05-15 21:27:36 +0200

    adding config

---
+++ MicroRabbit.Infra.IoC/DependencyContainer.cs
@@ -0,0 +1,17 @@
+﻿using MicroRabbit.Domain.Core.Bus;
+using MicroRabbit.Infra.Bus;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MicroRabbit.Infra.IoC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddTransient<IEventBus, RabbitMqBus>();
+
+            services.Configure<RabbitMqSettings>(c => configuration.GetSection("RabbitMqSettings"));
+        }
+    }
+}
